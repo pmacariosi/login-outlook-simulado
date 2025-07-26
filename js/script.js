0004ed7a8ed0e55582d438be7524ce9ce4471f36
pmacariosi <paulomacariosi@hotmail.com.br>
2025-07-26 18:38:03 -0300

    ✨ Estrutura modular simulador de login - HTML, CSS e JS

--- js/script.js
+++ js/script.js
@@ -2,13 +2,8 @@ function simularLogin() {
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
 
-  async function gerarHash(texto) {
-    const msgUint8 = new TextEncoder().encode(texto);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    alert(`Usuário: ${email}\nSenha em SHA-256: ${hashHex}`);
+  if (email && senha) {
+    alert(`Bem-vindo, ${email}!`);
+    window.location.href = "boas-vindas.html";
   }
-
-  gerarHash(senha);
 }
